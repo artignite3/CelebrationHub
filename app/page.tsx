099003324307dcd5bc29bee01ec44b1be34c401e
artignite3 <riddhu.work@gmail.com>
2025-10-24 20:08:59 +0530

    Descrptive

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import HeroSection from "@/components/hero-section"
 import GallerySection from "@/components/gallery-section"
 import FeaturesSection from "@/components/features-section"
 import FooterSection from "@/components/footer-section"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Home() {
   return (
